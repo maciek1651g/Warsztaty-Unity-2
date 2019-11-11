e1269785dadf5c0af7c8daaf254468fb0fc16726
Adam <grzybojad@gmail.com>
2019-11-11 17:08:01 +0100

    Added the Unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private int score = 0;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	// FixedUpdate is called every fixed frame-rate frame, use it when using Rigidbody
+	private void FixedUpdate()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter2D( Collision2D collision )
+	{
+		
+	}
+}
